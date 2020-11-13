c5289c093aa7d13d0da20c844f1645be5216b908
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-11-13 15:53:36 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan49-BiayaEmas/src/pboif2/pkg10119048/latihan49/biayaemas/PBOIF210119048Latihan49BiayaEmas.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan49.biayaemas;
+
+/**
+ *
+ * @author SystemOs
+ * Nama      : Mochammad Faishal
+ * NIM       : 10119048
+ * Kelas     : IF2
+ * Deskripsi : Menampilkan program Biaya emas kawin
+ */
+public class PBOIF210119048Latihan49BiayaEmas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Emas emas = new Emas(570000,15.7);
+        
+        emas.tampil();
+        System.out.println("==========================================");
+        System.out.println("Developed by : Mochammad Faishal");
+    }
+    
+}
